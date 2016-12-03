f808fb309d71884debc676ed834fdd9d125879e1
Adrian <adrianchung@hotmail.co.uk>
2016-12-03 22:40:59 +0000

    Added demo bot

---
+++ recapbot.py
@@ -0,0 +1,93 @@
+from itty import *
+import urllib2
+import requests
+import json
+
+
+def sendSparkGET(message_id):
+    """
+    This method is used for:
+        -retrieving message text, when the webhook is triggered with a message
+        -Getting the username of the person who posted the message if a command is recognized
+    """
+    # request = urllib2.Request('https://api.ciscospark.com/v1/messages/' + message_id,
+    #                         headers={"Accept" : "application/json",
+    #                                  "Content-Type":"application/json"})
+    # request.add_header("Authorization", "Bearer "+bearer)
+    # contents = urllib2.urlopen(request).read()
+    try:
+        response = requests.get(
+            url="https://api.ciscospark.com/v1/messages/" + message_id,
+            headers={
+                "Authorization": "Bearer <token>",
+                "Content-Type": "application/json; charset=utf-8",
+            },
+        )
+        print('Response HTTP Status Code: {status_code}'.format(
+            status_code=response.status_code))
+        print('Response HTTP Response Body: {content}'.format(
+            content=response.content))
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+    return response.content
+
+def sendSparkPOST(url, data):
+    """
+    This method is used for:
+        -posting a message to the Spark room to confirm that a command was received and processed
+    """
+    request = urllib2.Request(url, json.dumps(data),
+                            headers={"Accept" : "application/json",
+                                     "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents
+
+
+
+@post('/')
+def index(request):
+    """
+    When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
+    using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
+    further actions are taken. i.e.
+    /batman    - replies to the room with text
+    /batcave   - echoes the incoming text to the room
+    /batsignal - replies to the room with an image
+    """
+    webhook = json.loads(request.body)
+    print webhook['data']['id']
+    print('https://api.ciscospark.com/v1/messages/')
+    print('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
+    result = sendSparkGET(webhook['data']['id'])
+    result = json.loads(result)
+    print "JSON COMING >>>>>>>"
+    print result
+    msg = None
+    if webhook['data']['personEmail'] != bot_email:
+        in_message = result.get('text', '').lower()
+        in_message = in_message.replace(bot_name, '')
+        if 'batman' in in_message or "whoareyou" in in_message:
+            msg = "I'm Batman!"
+        elif 'batcave' in in_message:
+            message = result.get('text').split('batcave')[1].strip(" ")
+            if len(message) > 0:
+                msg = "The Batcave echoes, '{0}'".format(message)
+            else:
+                msg = "The Batcave is silent..."
+        elif 'batsignal' in in_message:
+            print "NANA NANA NANA NANA"
+            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "files": bat_signal})
+        if msg != None:
+            print msg
+            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
+    return "true"
+
+
+####CHANGE THESE VALUES#####
+bot_email = "recapbot@sparkbot.io"
+bot_name = "recapbot"
+bearer = "<token>"
+bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
+run_itty(server='wsgiref', host='127.0.0.1', port=8080)
