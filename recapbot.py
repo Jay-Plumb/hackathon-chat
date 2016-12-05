9c15d99381064e7042c11ae4410304438ea5f4c3
Adrian <adrianchung@hotmail.co.uk>
2016-12-05 01:13:37 +0000

    Added comments to the bot script and edited layout of readme

--- recapbot.py
+++ recapbot.py
@@ -1,11 +1,13 @@
 from itty import *
+from process_messages import process
 import urllib2
 import requests
 import json
-from process_messages import process
-def getMessages(room_id,):
-    print("ROOM:")
+
+#Gets messages (up to 1000 messages) from a chat room:
+def getMessages(room_id):
     try:
+        #GET request for obtaining all messages in a specific Spark room:
         response = requests.get(
             url="https://api.ciscospark.com/v1/messages/?roomId=" + room_id + "&max=1000",
             headers={
@@ -21,125 +23,59 @@ def getMessages(room_id,):
         print('HTTP Request failed')
     return response.content
 
-def postMessage(room_id, item):
-
-    try:
-        response = requests.post(
-            url="https://api.ciscospark.com/v1/messages/?roomId=" + room_id,
-            headers={
-                "Authorization": "Bearer " +bearer,
-                "Content-Type": "application/json; charset=utf-8"
-            }, data = {'markdown': item})
-        
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
-
-
-def sendSparkGET(message_id):
-    """
-    This method is used for:
-        -retrieving message text, when the webhook is triggered with a message
-        -Getting the username of the person who posted the message if a command is recognized
-    """
-    # request = urllib2.Request('https://api.ciscospark.com/v1/messages/' + message_id,
-    #                         headers={"Accept" : "application/json",
-    #                                  "Content-Type":"application/json"})
-    # request.add_header("Authorization", "Bearer "+bearer)
-    # contents = urllib2.urlopen(request).read()
-    try:
-        response = requests.get(
-            url="https://api.ciscospark.com/v1/messages/" + message_id,
-            headers={
-                "Authorization": "Bearer " +bearer,
-                "Content-Type": "application/json; charset=utf-8",
-            },
-        )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
-    except requests.exceptions.RequestException:
-        print('HTTP Request failed')
-
-    return response.content
-
 def sendSparkPOST(url, data):
     """
     This method is used for:
         -posting a message to the Spark room to confirm that a command was received and processed
     """
-    # print "DATA:"
-    # print data
     request = urllib2.Request(url, json.dumps(data),
                              headers={"Accept" : "application/json",
                                       "Content-Type":"application/json"})
     request.add_header("Authorization", "Bearer "+bearer)
     contents = urllib2.urlopen(request).read()
-    #print contents
     return contents
-
-
 
 @post('/')
 def index(request):
     """
     When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
-    using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
-    further actions are taken. i.e.
-    /batman    - replies to the room with text
-    /batcave   - echoes the incoming text to the room
-    /batsignal - replies to the room with an image
+    using the sendSparkGet() function.  The message text is parsed.
+
     """
     webhook = json.loads(request.body)
 
-    #print "web"
-    #print webhook['data']['roomId']
-    #print webhook['data']['id']
-    #print('https://api.ciscospark.com/v1/messages/')
-    #print('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
-
-    #result = sendSparkGET(webhook['data']['id'])
-    
+    #The following lines are not called if webhook was triggered by bot:
     if webhook["data"]["personEmail"] != bot_email:
         result = getMessages(webhook['data']['roomId'])
-    
 
-        # result = getMessages(webhook['data']['roomId'])
         data = []
         result = json.loads(result)
         filtered_data = {"items": data}
 
+        #'For' loop to filter out all messages outputted by the bot and that call the bot:
         for item in result["items"]:
-            
             if (item['personEmail'] != bot_email):
                 if (item['text'] != bot_name):
                     filtered_data["items"].append(item)
+        #Convert dictionary of filtered data into a prettified JSON file:
         with open('unprocessed.json', 'w') as outfile:
-            json.dump(filtered_data, outfile)
-       
+            json.dump(filtered_data, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
+        #Generate unique markdowns for the filtered messages and reverse their order, so that older messages are outputted first:
         messages = process('unprocessed.json')
         messages["items"].reverse()
+        #POST request to output all markdown text onto the Spark room:
         for item in messages["items"]:
-            print "DEBUG:"
-            #print item
             sendSparkPOST("https://api.ciscospark.com/v1/messages/", {"roomId": webhook['data']['roomId'], "markdown": item["markdown"]})
-            #break
-            #postMessage(webhook['data']['roomId'], item["markdown"]);
 
-
+        #Create a prettified JSON file for each message, storing the 'markdown' generated:
         with open('processed.json', 'w') as outfile:
-            json.dump(messages, outfile)
-    
+            json.dump(messages, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+
     return "true"
 
-
-####CHANGE THESE VALUES#####
+####Values of the bot#####
 bot_email = "recapbot@gmail.com"
 bot_name = "recapbot"
 bearer = "OGM2MjljMzMtZTZiYS00YTc2LWJmOTUtZGFjNWY0YmZkZDUwZTlkNTg2N2QtNWUy"
-bat_signal  = "https://upload.wikimedia.org/wikipedia/en/c/c6/Bat-signal_1989_film.jpg"
 run_itty(server='wsgiref', host='127.0.0.1', port=8080)
